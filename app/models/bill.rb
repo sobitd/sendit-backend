cc0a6e655de2eb3211e76b4068089c21ccdc14a7
sobitd <sobitdaniel@gmail.com>
2022-10-24 13:46:39 +0300

    Add testing and associations to Bill model

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,4 @@
 class Bill < ApplicationRecord
     has_one :delivery
+    validates :total_cost, presence: true
 end
--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Bill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do 
+    it { should have_one(:delivery)}
+  end
+
+  describe "Validations" do 
+    it { should validate_presence_of(:total_cost)}
+  end
 end
