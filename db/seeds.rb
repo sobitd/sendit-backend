6ef6513336e83eabed2a15f369c282d5d2619f65
korosdennis <deniptoo98@gmaiil.com>
2022-10-25 12:41:13 +0300

    users seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "seeding users...❄"
+User.create!([
+    {first_name:"dan",last_name:"mike",email_address:"dan@mail.com",phone_number:"1234512",username:"dan
+    ",password:"dan123"},
+    {first_name:"john",last_name:"doe",email_address:"john@mail.com",phone_number:"1234512",username:"john
+        ",password:"john123"},
+    {first_name:"jane",last_name:"lucy",email_address:"jane@mail.com",phone_number:"1234512",username:"jane
+        ",password:"jane123"}
+
+    ])
+
+
+    puts "done seeding...✅✅"
