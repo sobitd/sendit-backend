5ba167318a90535bba48db1c25e447aa3fb8e8c2
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-10-25 12:54:48 +0300

    created sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -98,7 +98,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_092219) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name_"
+    t.string "first_name"
     t.string "last_name"
     t.string "email_address"
     t.integer "phone_number"
