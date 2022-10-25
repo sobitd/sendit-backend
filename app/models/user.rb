33128af0d26bb050a596ee9a0c60b6f9da699cc7
sobitd <sobitdaniel@gmail.com>
2022-10-25 12:17:55 +0300

    add uniqueness validation to phone number in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :email_address, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
   number_regex = /\d[0-9]\)*\z/
   validates_format_of :phone_number, with: number_regex,
-                                     message: 'Only positive number without spaces are allowed'
+                                     message: 'Only positive number without spaces are allowed', uniqueness: true
   has_many :orders
   has_many :parcels, through: :orders
 end
