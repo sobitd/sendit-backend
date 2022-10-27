aee35546216790a7b4205da6ada48941ae899edc
sobitd <sobitdaniel@gmail.com>
2022-10-27 19:35:12 +0300

    seeding

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
   # root to: "main#index"
- 
-  get "/me", to: "users#show"
+
+  get '/me', to: 'users#show'
   root to: 'users#index'
   get '/signup', to: 'user#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
-  
+
   resources :sessions, only: [:create]
   resources :users do
     resources :parcels
   end
- end
+end
